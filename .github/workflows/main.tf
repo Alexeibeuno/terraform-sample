a955c4226724b1fe3705112093bf5c9c3b57292c
Alexei Beuno H <67053046+Alexeibeuno@users.noreply.github.com>
2025-03-25 08:22:41 +0530

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     snowflake = {
-      source  = "chanzuckerberg/snowflake"
-      version = "0.25.17"
+      source  = "registry.terraform.io/snowflake-Labs/snowflake"
+      version = "~> 0.87"
     }
   }
 
