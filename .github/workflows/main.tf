8db9b25758e54934830ff8c78a29e15393c14138
Alexei Beuno H <67053046+Alexeibeuno@users.noreply.github.com>
2025-03-31 18:33:35 +0530

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -19,10 +19,10 @@ provider "snowflake" {
 organization_name = "PBXUDTW" # required if not using profile. Can also be set via SNOWFLAKE_ORGANIZATION_NAME env var
 account_name      = "VY63582" # required if not using profile. Can also be set via SNOWFLAKE_ACCOUNT_NAME env var
 user              = "HALEXE" # required if not using profile or token. Can also be set via SNOWFLAKE_USER env var
-password          = "Senorita@b0211"
+password          = "J2ZA.e4TNHuWw_B"
 }
 
-resource "snowflake_schema" "DEV_1_Schema" {
+resource "snowflake_schema" "demo_schema" {
   database = snowflake_database.DEV_1.name
   name     = "DEV1_SCHEMA"
   comment  = "Schema for Snowflake Terraform demo"
