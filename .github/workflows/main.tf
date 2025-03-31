c4e2025485ae64a91c18d625b25501ce8945102c
Alexei Beuno H <67053046+Alexeibeuno@users.noreply.github.com>
2025-03-31 18:54:21 +0530

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -27,7 +27,7 @@ resource "snowflake_database" "demo_db" {
 }
 
 resource "snowflake_schema" "demo_schema" {
-  database = snowflake_database.DEV1.name
+  database = snowflake_database.demo_db.name
   name     = "DEV1_Schema"
   comment  = "Schema for Snowflake Terraform demo"
 }
