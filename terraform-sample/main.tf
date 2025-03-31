4ab8574dc9a53e9b88e3a65ff579b14643e406b2
Alexeibeuno <alexeibeuno2000@gmail.com>
2025-03-31 19:23:40 +0530

    Testing the PR for Advanced Workflow

--- terraform-sample/main.tf
+++ terraform-sample/main.tf
@@ -5,97 +5,29 @@ terraform {
       version = "~> 0.87"
     }
   }
+
+  backend "remote" {
+    organization = "Snow-Git-Terr"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
 }
 
 provider "snowflake" {
-  account_name = "VY63582"
-organization_name = "PBXUDTW"
-  user = "HALEXE"
-  password = "Senorita@b0211"
-  role = "SYSADMIN"
+organization_name = "PBXUDTW" # required if not using profile. Can also be set via SNOWFLAKE_ORGANIZATION_NAME env var
+account_name      = "VY63582" # required if not using profile. Can also be set via SNOWFLAKE_ACCOUNT_NAME env var
+user              = "HALEXE" # required if not using profile or token. Can also be set via SNOWFLAKE_USER env var
+password          = "J2ZA.e4TNHuWw_B"
 }
 
-resource "snowflake_database" "db" {
-  name = "TF_DEMO"
+resource "snowflake_database" "demo_db" {
+   name = "DEV1"
 }
 
-resource "snowflake_warehouse" "warehouse" {
-  name           = "TF_DEMO"
-  warehouse_size = "xsmall"
-  auto_suspend   = 60
-}
-
-provider "snowflake" {
-  alias = "security_admin"
-  role  = "SECURITYADMIN"
-}
-
-resource "snowflake_role" "role" {
-  provider = snowflake.security_admin
-  name     = "TF_DEMO_SVC_ROLE"
-}
-
-resource "snowflake_grant_privileges_to_account_role" "database_grant" {
-  provider          = snowflake.security_admin
-  privileges        = ["USAGE"]
-  account_role_name = snowflake_role.role.name
-  on_account_object {
-    object_type = "DATABASE"
-    object_name = snowflake_database.db.name
-  }
-}
-
-resource "snowflake_schema" "schema" {
-  database   = snowflake_database.db.name
-  name       = "TF_DEMO"
-  is_managed = false
-}
-
-resource "snowflake_grant_privileges_to_account_role" "schema_grant" {
-  provider          = snowflake.security_admin
-  privileges        = ["USAGE"]
-  account_role_name = snowflake_role.role.name
-  on_schema {
-    schema_name = "\"${snowflake_database.db.name}\".\"${snowflake_schema.schema.name}\""
-  }
-}
-
-resource "snowflake_grant_privileges_to_account_role" "warehouse_grant" {
-  provider          = snowflake.security_admin
-  privileges        = ["USAGE"]
-  account_role_name = snowflake_role.role.name
-  on_account_object {
-    object_type = "WAREHOUSE"
-    object_name = snowflake_warehouse.warehouse.name
-  }
-}
-
-resource "tls_private_key" "svc_key" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
-
-resource "snowflake_user" "user" {
-    provider          = snowflake.security_admin
-    name              = "tf_demo_user"
-    default_warehouse = snowflake_warehouse.warehouse.name
-    default_role      = snowflake_role.role.name
-    default_namespace = "${snowflake_database.db.name}.${snowflake_schema.schema.name}"
-    rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
-}
-
-resource "snowflake_grant_privileges_to_account_role" "user_grant" {
-  provider          = snowflake.security_admin
-  privileges        = ["MONITOR"]
-  account_role_name = snowflake_role.role.name  
-  on_account_object {
-    object_type = "USER"
-    object_name = snowflake_user.user.name
-  }
-}
-
-resource "snowflake_grant_account_role" "grants" {
-  provider  = snowflake.security_admin
-  role_name = snowflake_role.role.name
-  user_name = snowflake_user.user.name
+resource "snowflake_schema" "demo_schema" {
+  database = snowflake_database.demo_db.name
+  name     = "DEV1_Schema_3"
+  comment  = "Schema for Snowflake Terraform demo"
 }
