8531695b0f2e077db4ae76839931783e687b1516
Alexeibeuno <alexeibeuno2000@gmail.com>
2025-03-31 19:55:41 +0530

    Testing the columns addition and pull request part

--- terraform-sample/main.tf
+++ terraform-sample/main.tf
@@ -28,6 +28,122 @@ resource "snowflake_database" "demo_db" {
 
 resource "snowflake_schema" "demo_schema" {
   database = snowflake_database.demo_db.name
-  name     = "DEV1_Schema_Demo"
-  comment  = "Schema for Snowflake Terraform demo"
+  name     = "DEV1_Schema_2"
+}
+
+resource "snowflake_table" "demo_table" {
+database = snowflake_database.demo_db.name
+schema = snowflake_database.demo_db.demo_schema.name
+name = "TB_PIPELINE_METADATA"
+comment = "Table created for Terrform Demo"
+change_tracking = false
+column {
+name = "PIPELINE_ID"
+type = "int"
+nullable = true
+default {
+sequence = snowflake_sequence.seq_seqnm.fully_qualified_name
+}
+}
+column {
+name = "LOB"
+type = "text"
+nullable = true
+}
+}
+column {
+name = "SOURCE"
+type = "text"
+nullable = true
+}
+column {
+name = "SOURCE_DATABASE"
+type = "text"
+nullable = true
+}
+column {
+name = "SOURCE_SCHEMA"
+type = "text"
+nullable = true
+}
+column {
+name = "SOURCE_STAGE"
+type = "text"
+nullable = true
+}
+column {
+name = "SOURCE_FILEFORMAT"
+type = "text"
+nullable = true
+}
+column {
+name = "SOURCE_RELATIVE_FILE_PATH"
+type = "text"
+nullable = true
+}
+column {
+name = "SOURCE_FILE_REGEX"
+type = "text"
+nullable = true
+}
+column {
+name = "SOURCE_FILE_TYPE"
+type = "text"
+nullable = true
+}
+column {
+name = "SOURCE_SCHEMATA"
+type = "text"
+nullable = true
+}
+column {
+name = "LANDING_DATABASE"
+type = "text"
+nullable = true
+}
+column {
+name = "LANDING_SCHEMA"
+type = "text"
+nullable = true
+}
+column {
+name = "LANDING_TABLE"
+type = "text"
+nullable = true
+}
+column {
+name = "TARGET_DATABASE"
+type = "text"
+nullable = true
+}
+column {
+name = "TARGET_SCHEMA"
+type = "text"
+nullable = true
+}
+column {
+name = "TARGET_TABLE"
+type = "text"
+nullable = true
+}
+column {
+name = "LOAD_TYPE"
+type = "text"
+nullable = true
+}
+column {
+name = "LOAD_STATUS"
+type = "text"
+nullable = true
+}
+column {
+name = "INS_TMESTMP"
+type = "text"
+nullable = true
+}
+column {
+name = "COMMENTS"
+type = "text"
+nullable = true
+}
 }
