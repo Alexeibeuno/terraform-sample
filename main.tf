46ba4af47ed8a133460debc58e3f0f49d7314748
Alexei Beuno H <67053046+Alexeibeuno@users.noreply.github.com>
2025-04-06 12:34:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,10 +23,126 @@ password          = "J2ZA.e4TNHuWw_B"
 }
 
 resource "snowflake_database" "demo_db" {
-   name = "DEV1_PR"
+   name = "DEV1"
 }
 
 resource "snowflake_schema" "demo_schema" {
   database = snowflake_database.demo_db.name
-  name     = "DEV1_Schema_1_PR"
+  name     = "DEV1_Schema_2"
+}
+
+resource "snowflake_table" "demo_table" {
+database = snowflake_database.demo_db.name
+schema = snowflake_database.demo_db.demo_schema.name
+name = "TB_PIPELINE_METADATA"
+comment = "Table created for Terrform Demo"
+change_tracking = false
+column {
+name = "PIPELINE_ID"
+type = "int"
+nullable = true
+default {
+sequence = snowflake_sequence.seq_seqnm.fully_qualified_name
+}
+}
+column {
+name = "LOB"
+type = "text"
+nullable = true
+}
+}
+column {
+name = "SOURCE"
+type = "text"
+nullable = true
+}
+column {
+name = "SOURCE_DATABASE_1"
+type = "text"
+nullable = true
+}
+column {
+name = "SOURCE_SCHEMA_1"
+type = "text"
+nullable = true
+}
+column {
+name = "SOURCE_STAGE"
+type = "text"
+nullable = true
+}
+column {
+name = "SOURCE_FILEFORMAT"
+type = "text"
+nullable = true
+}
+column {
+name = "SOURCE_RELATIVE_FILE_PATH"
+type = "text"
+nullable = true
+}
+column {
+name = "SOURCE_FILE_REGEX"
+type = "text"
+nullable = true
+}
+column {
+name = "SOURCE_FILE_TYPE"
+type = "text"
+nullable = true
+}
+column {
+name = "SOURCE_SCHEMATA"
+type = "text"
+nullable = true
+}
+column {
+name = "LANDING_DATABASE"
+type = "text"
+nullable = true
+}
+column {
+name = "LANDING_SCHEMA"
+type = "text"
+nullable = true
+}
+column {
+name = "LANDING_TABLE"
+type = "text"
+nullable = true
+}
+column {
+name = "TARGET_DATABASE"
+type = "text"
+nullable = true
+}
+column {
+name = "TARGET_SCHEMA"
+type = "text"
+nullable = true
+}
+column {
+name = "TARGET_TABLE"
+type = "text"
+nullable = true
+}
+column {
+name = "LOAD_TYPE"
+type = "text"
+nullable = true
+}
+column {
+name = "LOAD_STATUS"
+type = "text"
+nullable = true
+}
+column {
+name = "INS_TMESTMP"
+type = "text"
+nullable = true
+}
+column {
+name = "COMMENTS"
+type = "text"
+nullable = true
 }
