8ec81382e31b4fd38077eb0340dbf7f49ccb8200
Alexei Beuno H <67053046+Alexeibeuno@users.noreply.github.com>
2025-04-06 12:41:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,14 +16,14 @@ terraform {
 }
 
 provider "snowflake" {
-organization_name = "PBXUDTW" # required if not using profile. Can also be set via SNOWFLAKE_ORGANIZATION_NAME env var
-account_name      = "VY63582" # required if not using profile. Can also be set via SNOWFLAKE_ACCOUNT_NAME env var
-user              = "HALEXE" # required if not using profile or token. Can also be set via SNOWFLAKE_USER env var
-password          = "J2ZA.e4TNHuWw_B"
+  organization_name = "PBXUDTW"  # required if not using profile. Can also be set via SNOWFLAKE_ORGANIZATION_NAME env var
+  account_name      = "VY63582"  # required if not using profile. Can also be set via SNOWFLAKE_ACCOUNT_NAME env var
+  user              = "HALEXE"   # required if not using profile or token. Can also be set via SNOWFLAKE_USER env var
+  password          = "J2ZA.e4TNHuWw_B"
 }
 
 resource "snowflake_database" "demo_db" {
-   name = "DEV1"
+  name = "DEV1"
 }
 
 resource "snowflake_schema" "demo_schema" {
@@ -32,117 +32,120 @@ resource "snowflake_schema" "demo_schema" {
 }
 
 resource "snowflake_table" "demo_table" {
-database = snowflake_database.demo_db.name
-schema = snowflake_database.demo_db.demo_schema.name
-name = "TB_PIPELINE_METADATA"
-comment = "Table created for Terrform Demo"
-change_tracking = false
-column {
-name = "PIPELINE_ID"
-type = "int"
-nullable = true
-default {
-sequence = snowflake_sequence.seq_seqnm.fully_qualified_name
-}
-}
-column {
-name = "LOB"
-type = "text"
-nullable = true
-}
-}
-column {
-name = "SOURCE"
-type = "text"
-nullable = true
-}
-column {
-name = "SOURCE_DATABASE_1"
-type = "text"
-nullable = true
-}
-column {
-name = "SOURCE_SCHEMA_1"
-type = "text"
-nullable = true
-}
-column {
-name = "SOURCE_STAGE"
-type = "text"
-nullable = true
-}
-column {
-name = "SOURCE_FILEFORMAT"
-type = "text"
-nullable = true
-}
-column {
-name = "SOURCE_RELATIVE_FILE_PATH"
-type = "text"
-nullable = true
-}
-column {
-name = "SOURCE_FILE_REGEX"
-type = "text"
-nullable = true
-}
-column {
-name = "SOURCE_FILE_TYPE"
-type = "text"
-nullable = true
-}
-column {
-name = "SOURCE_SCHEMATA_1"
-type = "text"
-nullable = true
-}
-column {
-name = "LANDING_DATABASE"
-type = "text"
-nullable = true
-}
-column {
-name = "LANDING_SCHEMA"
-type = "text"
-nullable = true
-}
-column {
-name = "LANDING_TABLE"
-type = "text"
-nullable = true
-}
-column {
-name = "TARGET_DATABASE"
-type = "text"
-nullable = true
-}
-column {
-name = "TARGET_SCHEMA"
-type = "text"
-nullable = true
-}
-column {
-name = "TARGET_TABLE"
-type = "text"
-nullable = true
-}
-column {
-name = "LOAD_TYPE"
-type = "text"
-nullable = true
-}
-column {
-name = "LOAD_STATUS"
-type = "text"
-nullable = true
-}
-column {
-name = "INS_TMESTMP"
-type = "text"
-nullable = true
-}
-column {
-name = "COMMENTS"
-type = "text"
-nullable = true
+  database = snowflake_database.demo_db.name
+  schema   = snowflake_schema.demo_schema.name
+  name     = "TB_PIPELINE_METADATA"
+  comment  = "Table created for Terraform Demo"
+  change_tracking = false
+
+  columns {
+    column {
+      name    = "PIPELINE_ID"
+      type    = "int"
+      nullable = true
+      default {
+        sequence = snowflake_sequence.seq_seqnm.fully_qualified_name
+      }
+    }
+    column {
+      name    = "LOB"
+      type    = "text"
+      nullable = true
+    }
+    column {
+      name    = "SOURCE"
+      type    = "text"
+      nullable = true
+    }
+    column {
+      name    = "SOURCE_DATABASE_1"
+      type    = "text"
+      nullable = true
+    }
+    column {
+      name    = "SOURCE_SCHEMA_1"
+      type    = "text"
+      nullable = true
+    }
+    column {
+      name    = "SOURCE_STAGE"
+      type    = "text"
+      nullable = true
+    }
+    column {
+      name    = "SOURCE_FILEFORMAT"
+      type    = "text"
+      nullable = true
+    }
+    column {
+      name    = "SOURCE_RELATIVE_FILE_PATH"
+      type    = "text"
+      nullable = true
+    }
+    column {
+      name    = "SOURCE_FILE_REGEX"
+      type    = "text"
+      nullable = true
+    }
+    column {
+      name    = "SOURCE_FILE_TYPE"
+      type    = "text"
+      nullable = true
+    }
+    column {
+      name    = "SOURCE_SCHEMATA_1"
+      type    = "text"
+      nullable = true
+    }
+    column {
+      name    = "LANDING_DATABASE"
+      type    = "text"
+      nullable = true
+    }
+    column {
+      name    = "LANDING_SCHEMA"
+      type    = "text"
+      nullable = true
+    }
+    column {
+      name    = "LANDING_TABLE"
+      type    = "text"
+      nullable = true
+    }
+    column {
+      name    = "TARGET_DATABASE"
+      type    = "text"
+      nullable = true
+    }
+    column {
+      name    = "TARGET_SCHEMA"
+      type    = "text"
+      nullable = true
+    }
+    column {
+      name    = "TARGET_TABLE"
+      type    = "text"
+      nullable = true
+    }
+    column {
+      name    = "LOAD_TYPE"
+      type    = "text"
+      nullable = true
+    }
+    column {
+      name    = "LOAD_STATUS"
+      type    = "text"
+      nullable = true
+    }
+    column {
+      name    = "INS_TMESTMP"
+      type    = "text"
+      nullable = true
+    }
+    column {
+      name    = "COMMENTS"
+      type    = "text"
+      nullable = true
+    }
+  }
 }
